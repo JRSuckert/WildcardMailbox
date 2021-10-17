1acb02b9ee06f43b3a229a7e1ef8660bf7f124a9
Jens Rene Suckert <jens.suckert@gmail.com>
2021-10-17 15:49:06 +0200

    PutItem into DynamoDB, still hard coded

--- lambda/index.js
+++ lambda/index.js
@@ -1,40 +1,68 @@
 var AWS = require('aws-sdk');
 var s3 = new AWS.S3();
-// Create DynamoDB document client
-var docClient = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
-
-var bucketName = process.env.BUCKET_NAME;
-var domain = process.env.DOMAIN;
+var ddb = new AWS.DynamoDB.DocumentClient();
+ 
+var bucketName = 'mail-leswile.de';
  
 exports.handler = function(event, context, callback) {
-    event.Records.foreach(record => {
+    if(!Array.isArray(event.Records) || event.Records.length === 0) {
+        return;
+    }
+    
+    for(let record of event.Records) {
+        if(record.eventSource !== "aws:ses") {
+            return;
+        }
+        console.log('Process email');
+        console.log("SES Notification:\n", JSON.stringify(record));
         var sesNotification = record.ses;
-        const base = sesNotification.mail.destination.filter(address => address.endsWith(`@${domain}`))[0];
+        const base = sesNotification.mail.destination.filter(address => 
+            address.endsWith("@leswile.de"))[0];
         const folder = sesNotification.mail.source;
-        const subject = encodeURI(sesNotification.mail.commonHeaders.subject);
+        const subjectEncoded = encodeURI(sesNotification.mail.commonHeaders.subject);
         const id = sesNotification.mail.messageId;
-        // put id sender, receiver, subject into dynamoDB
-        // send notification to user
-    });
+        const key = base + "/" + folder + "/" + subjectEncoded + "-" + id + ".eml";
 
+        var params = {
+            Bucket: bucketName, 
+            CopySource: "/mail-leswile.de/"+id, 
+            Key: key
+        };
+        s3.copyObject(params, function(err, data) {
+            if (err) console.log(err, err.stack); // an error occurred
+            else {
+                var params = {
+                    Bucket: bucketName, 
+                    Key: id
+                };
+                s3.deleteObject(params, function(err, data) {
+                    if (err) console.log(err, err.stack); // an error occurred
+                    else     console.log(data);           // successful response
+                });
+            }
+        });
+        
+        const timestamp = Date.parse(sesNotification.mail.timestamp);
+        const headers = sesNotification.mail.headers;
+        const recipient = headers.find(header => header.name === "To");
+        const sender = headers.find(header => header.name === "From");
+        const subject = headers.find(header => header.name === "Subject");
+        
+        if(id && timestamp && recipient && sender && subject && key) {
+            ddb.put({
+                Item: {
+                    uuid: id,
+                    date: timestamp,
+                    recipient: recipient.value,
+                    sender: sender.value,
+                    subject: subject.value,
+                    key
+                },
+                TableName: "mail-leswile.de",
+            }).promise()
+            .then(data => console.log(data.Attributes))
+            .catch(console.error);
+        }
+    }
     
 };
-
-function putItem() {
-    var params = {
-    TableName: process.env.TABLE_NAME,
-    Item: {
-        'HASHKEY': VALUE,
-        'ATTRIBUTE_1': 'STRING_VALUE',
-        'ATTRIBUTE_2': VALUE_2
-    }
-    };
-
-    docClient.put(params, function(err, data) {
-    if (err) {
-        console.log("Error", err);
-    } else {
-        console.log("Success", data);
-    }
-    });
-}
